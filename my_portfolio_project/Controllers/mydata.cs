f0afeff3322329562b7bd138a5c8771ebd9a24eb
Oluwaseyi Fatunmole <seyiara1@yahoo.com>
2024-03-10 12:38:13 +0100

    added modelstate validation

--- my_portfolio_project/Controllers/mydata.cs
+++ my_portfolio_project/Controllers/mydata.cs
@@ -11,11 +11,10 @@ namespace my_portfolio_project.Controllers
 {
     // [Route("api/[controller]/user")]
     [Route("api/user")]
-
-    [ApiController]
+    //  [ApiController]
     public class Mydata : ControllerBase
     {
-        [HttpGet]
+        [HttpGet(Name = "GetAllUsers")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IEnumerable<UserDto>> GetAllUserAccount()
@@ -25,7 +24,7 @@ namespace my_portfolio_project.Controllers
             return Ok(dataList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetUserbyId")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserDto))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -47,36 +46,91 @@ namespace my_portfolio_project.Controllers
             return Ok(response);
         }
 
-        [HttpPost]
+        [HttpPost(Name = "CreateUser")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserDto))]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(UserDto))]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public ActionResult<UserDto> CreateNewUser([FromBody] UserDto _newUser)
         {
             var dataList = UserRepo.userRepo;
+
+            //check for model state validations
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //check if username provided is unique
+            if (
+                dataList.FirstOrDefault(x => x.UserName.ToLower() == _newUser.UserName.ToLower())
+                != null
+            )
+            {
+                ModelState!.AddModelError("Exception", "Username already exists");
+                return BadRequest(ModelState);
+            }
+
+            //check if username is null
             if (_newUser == null)
             {
                 return BadRequest(_newUser);
             }
 
+            //check if userid is not 0
             if (_newUser.id > 0)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
+            //check if userID exists
+            if (dataList.FirstOrDefault(x => x.id == _newUser.id) != null)
+            {
+                return BadRequest(_newUser);
+            }
+
+            //on success call
+            _newUser.id = dataList.OrderByDescending(x => x.id).FirstOrDefault()!.id + 1;
+            dataList.Add(_newUser);
+            return CreatedAtRoute("GetUserbyId", new { id = _newUser.id }, _newUser);
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteUser")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserDto))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<UserDto> DeleteUserById(int? id)
+        {
+            var dataList = UserRepo.userRepo;
+
+            if (id == null || id.ToString() == "")
+            {
+                ModelState.AddModelError("Exception", "id field must not be empty");
+
+                return BadRequest(ModelState);
+            }
+
+            var item = dataList.FirstOrDefault(x => x.id == id);
+
+            if (item == null)
+            {
+                ModelState.AddModelError("Exception", "This customer does not exist");
+                return NotFound(ModelState);
+            }
+
             for (var i = 0; i < dataList.Count; i++)
             {
-                if (_newUser.id == dataList[i].id)
+                if (id == dataList[i].id)
                 {
-                    return BadRequest(_newUser);
+                    dataList.RemoveAt(i);
                 }
             }
 
-
-            _newUser.id = dataList.OrderByDescending(x => x.id).FirstOrDefault()!.id + 1;
-            dataList.Add(_newUser);
-            return Ok(_newUser);
+            return Ok();
         }
     }
 }
--- my_portfolio_project/Models/DTOs/UserDto.cs
+++ my_portfolio_project/Models/DTOs/UserDto.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace my_portfolio_project.Models.DTOs
 {
     public class UserDto
     {
+        [Required]
+        //[MaxLength(10)]
         public required string UserName { get; set; }
+
         public int id { get; set; }
+
         public int CustomerId { get; set; }
+
         public DateTime CreatedDate { get; set; }
+
     }
 }
